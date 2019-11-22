14a5efc9f512fccc6ab47c68b0df39f4c691abea
Justus Post <JPost@lcs.on.ca>
2019-11-22 14:33:33 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Justus Post on 2019-11-22.
+//  Copyright © 2019 Justus Post. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
